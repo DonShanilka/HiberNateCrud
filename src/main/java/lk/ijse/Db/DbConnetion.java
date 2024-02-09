ddc59ad34c5ff39b734dd9d76cbc5052b1c678d2
Shanilka <shanilka799@gmail.com>
2024-02-09 11:56:29 +0530

    DbConnection

--- src/main/java/lk/ijse/Db/DbConnetion.java
+++ src/main/java/lk/ijse/Db/DbConnetion.java
@@ -1,12 +1,25 @@
 package lk.ijse.Db;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DbConnetion {
     private static Connection connection;
     private static DbConnetion dbConnetion;
 
-    private DbConnetion(){
-
+    private DbConnetion() throws SQLException {
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/hibercrud",
+                "root",
+                "Shanilka800@#");
     }
+
+    public static DbConnetion getInstance() throws SQLException {
+        return (null == dbConnetion) ? new DbConnetion(): dbConnetion;
+    }
+
+    public Connection getConnection(){
+        return connection;
+    }
+
 }
