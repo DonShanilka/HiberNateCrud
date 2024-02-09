1c980714c35a0f26892b404fbca4acd4d42d14f9
Shanilka <shanilka799@gmail.com>
2024-02-09 11:44:10 +0530

    class

---
+++ src/main/java/lk/ijse/Db/DbConnetion.java
@@ -0,0 +1,12 @@
+package lk.ijse.Db;
+
+import java.sql.Connection;
+
+public class DbConnetion {
+    private static Connection connection;
+    private static DbConnetion dbConnetion;
+
+    private DbConnetion(){
+
+    }
+}
